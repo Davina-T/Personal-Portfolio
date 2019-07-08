15950cd563c9c6cefac091e0d6f2afb014abe753
Davina Tan <davina.tan@connect.qut.edu.au>
2019-07-09 01:43:24 +1000

    edited description, updated resume, added more social links

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
 // Contact Link Hover Effects
 
-$(".social-icon--email").mouseenter(function() {
-    $(".fa-envelope").addClass("colour");
-    $(".tooltip").css('visibility: visible; opacity: 1; transform: translate(-50%, -150%)');
-}).mouseleave(function() {
-    $(".fa-envelope").removeClass("colour");
-});
+// email
+$('.social-icon--email')
+	.mouseenter(function() {
+		$('.fa-envelope').addClass('colour')
+		$('.tooltip').css('visibility: visible; opacity: 1; transform: translate(-50%, -150%)')
+	})
+	.mouseleave(function() {
+		$('.fa-envelope').removeClass('colour')
+	})
 
-$(".social-icon--github").mouseenter(function() {
-    $(".fa-github").addClass("colour");
-}).mouseleave(function() {
-    $(".fa-github").removeClass("colour");
-});
+// github
+$('.social-icon--github')
+	.mouseenter(function() {
+		$('.fa-github').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.fa-github').removeClass('colour')
+	})
 
-$(".social-icon--linkedin").mouseenter(function() {
-    $(".fa-linkedin-in").addClass("colour");
-}).mouseleave(function() {
-    $(".fa-linkedin-in").removeClass("colour");
-});
+// linkedin
+$('.social-icon--linkedin')
+	.mouseenter(function() {
+		$('.fa-linkedin-in').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.fa-linkedin-in').removeClass('colour')
+	})
+
+// dribbble
+$('.social-icon--dribbble')
+	.mouseenter(function() {
+		$('.fa-dribbble').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.fa-dribbble').removeClass('colour')
+	})
+
+// instagram
+$('.social-icon--instagram')
+	.mouseenter(function() {
+		$('.fa-instagram').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.fa-instagram').removeClass('colour')
+	})
 
 // Tooltip
 
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip()
+})
